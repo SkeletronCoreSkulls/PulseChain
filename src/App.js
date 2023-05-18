320b44f02fa0b9f85979e347537ff14f75336c31
Skeleton Dev <128191216+SkeletronCoreSkulls@users.noreply.github.com>
2023-05-17 22:59:11 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ function App() {
     let gasLimit = CONFIG.GAS_LIMIT;
     let totalCostWei = String(cost * mintAmount);
     let totalGasLimit = String(gasLimit * mintAmount);
-    console.log("Cost: ", totalCostWei);
+    console.log("Cost: ", BigInt(totalCostWei));
     console.log("Gas limit: ", totalGasLimit);
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
